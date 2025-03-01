44f013ea87edd4fa4d0e7bca07517925efe365e1
geeinf <veelove1034@gmail.com>
2025-03-02 00:01:57 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1IwmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.wooomooo.com"];
+pub const RS_PUB_KEY: &str = "JJIiSb5HwF3kLZvQoo8gt3LfzjYW87Fgft3OKcV2fOU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
