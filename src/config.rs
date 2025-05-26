e41c639b235daf14299deb21eeca567594e0a916
Abcd9183 <as-dftyui@163.com>
2025-05-26 22:32:12 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["n.wooomooo.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsrryw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["74391l8b3.zicp.vip"];
+pub const RS_PUB_KEY: &str = "Z4sTiaVFOBQLGOPA8NoMcbVG4lufNcAnlSMurJBca+k=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
